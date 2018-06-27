db11913ebd1c151634b33af0108edf3bbb9a82f2
cc6688211 <39253243+cc6688211@users.noreply.github.com>
2018-06-27 17:15:44 +0800

    消费者-feign

--- feign/src/main/java/com/tit/springCloud/feign/test/service/TestService.java
+++ feign/src/main/java/com/tit/springCloud/feign/test/service/TestService.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//根据服务名称来指定服务提供方
 @FeignClient("eureka-client")
 public interface TestService {
+	// 通过注解指定访问方式，访问路径，访问参数
 	@RequestMapping(method = RequestMethod.GET, value = "/add")
 	Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
 
