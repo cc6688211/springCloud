41699f4809e6e34ab946d6ab5e77a50f9da50471
cc6688211 <39253243+cc6688211@users.noreply.github.com>
2018-07-02 10:49:52 +0800

    健康检查-eureka

---
+++ eurekaDiscoveryHealth/src/main/java/com/tit/eurekaDiscoveryHealth/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.tit.eurekaDiscoveryHealth.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@Autowired
+	DiscoveryClient discoveryClient;
+
+	@GetMapping("/dc")
+	public String dc() {
+		String services = "Services: " + discoveryClient.getServices() + new Date().getTime();
+		System.out.println(services);
+		return services;
+	}
+
+	@GetMapping("/add")
+	public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+		System.out.println(a + b);
+		return a + b;
+	}
+
+	@GetMapping("/stop")
+	public String stop() throws InterruptedException {
+		System.out.println("stop");
+		Thread.sleep(5000L);
+		return "stop";
+	}
+}
