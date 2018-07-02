7e20b29ca5ea94b318a85c76b670de5db4e39938
cc6688211 <39253243+cc6688211@users.noreply.github.com>
2018-07-02 14:39:16 +0800

    全链路追踪-服务提供者

---
+++ sleuthEurekaDiscovery/src/main/java/com/tit/springCloud/sleuthEurekaDiscovery/controller/TestLogController.java
@@ -0,0 +1,17 @@
+package com.tit.springCloud.sleuthEurekaDiscovery.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestLogController {
+	protected final static Logger logger = LoggerFactory.getLogger(TestLogController.class);
+
+	@GetMapping("/infoLog-1")
+	public String infoLog() {
+		logger.info("------infoLog-1------");
+		return "infoLog";
+	}
+}
